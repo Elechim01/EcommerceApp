6757c077f86a9975e014eeddfccb1e410706f2fd
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-11-27 23:59:27 +0100

    Initial Commit

---
+++ Shared/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 27/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
